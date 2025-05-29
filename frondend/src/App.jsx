35f782f5bd7db1d0edee393297de4970774b9f45
linhvo123 <vodinhlinh2058@gmail.com>
2025-05-29 20:36:16 +0700

    WIP: sửa nhiều file local

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,14 +1,32 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { Layout } from 'antd';
+import DashboardPage from './app/DashboardPage';
+import Tracking from "./app/Tracking"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+
+// gọi layout
+const { Content } = Layout;
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Router>
+      <Layout>
+        <Header />
+        <Content className="dashboard-content-wrapper">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/tracking" element={<Tracking />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </Layout>
+    </Router>
     </>
   );
 }
 
 export default App;
+ 
