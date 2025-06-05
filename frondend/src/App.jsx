46c00ea3e2223cfd2b5f93abb86571e218fe36b1
LƯU MỸ NHẬT HUY <huylmnse181744@fpt.edu.vn>
2025-06-05 07:28:31 +0700

    sửa App.jsx

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -5,35 +5,29 @@ import Header from "./components/Header";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./app/Login";
 import Register from "./app/Register";
-import Home from "./app/Home";
 import Tracking from "./app/Tracking";
 import DashboardPage from "./app/DashboardPage";
-import { Layout } from "antd";
 import HomePage from "./app/HomePage";
 import Plan from "./app/Plan";
 import CommunityBlogPage from "./app/CommunityBlogPage"; // Thêm import cho CommunityBlogPage
 import LeaderboardPage from "./app/LeaderboardPage";
 
 function App() {
-  const { Content } = Layout;
   return (
     <BrowserRouter>
       <AuthProvider>
         <Header />
-
         <Routes>
           <Route path="/tracking" element={<Tracking />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/plan" element={<Plan />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/blog" element={<CommunityBlogPage />} />
           <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
-
         <Footer />
       </AuthProvider>
     </BrowserRouter>
