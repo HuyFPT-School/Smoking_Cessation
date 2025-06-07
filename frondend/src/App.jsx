5fbdbf9039d428985c353c8727b9b25689743674
linhvo123 <vodinhlinh2058@gmail.com>
2025-06-07 15:06:35 +0700

    Thêm route cho CoachChat

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -5,34 +5,32 @@ import Header from "./components/Header";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./app/Login";
 import Register from "./app/Register";
-import Home from "./app/Home";
 import Tracking from "./app/Tracking";
 import DashboardPage from "./app/DashboardPage";
-import { Layout } from "antd";
 import HomePage from "./app/HomePage";
 import Plan from "./app/Plan";
 import CommunityBlogPage from "./app/CommunityBlogPage"; // Thêm import cho CommunityBlogPage
 import LeaderboardPage from "./app/LeaderboardPage";
-
+import Profile from "./app/Profile";
+import CoachChat from "./app/CoachChat"; // 6/7/2025 thêm import cho CoachChat
 function App() {
-  const { Content } = Layout;
   return (
     <BrowserRouter>
       <AuthProvider>
         <Header />
-
         <Routes>
           <Route path="/tracking" element={<Tracking />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/plan" element={<Plan />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/blog" element={<CommunityBlogPage />} />
+          <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/coachchat" element={<CoachChat />} />
         </Routes>
-
         <Footer />
       </AuthProvider>
     </BrowserRouter>
